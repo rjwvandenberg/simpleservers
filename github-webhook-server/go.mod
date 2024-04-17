6362e5be7f1e4b4fab4710d9d351ebb31d8196df
René van den Berg <rjwvandenberg@gmail.com>
2024-04-17 12:13:01 +0200

    Fix servers-publish workflow permissions

---
+++ github-webhook-server/go.mod
@@ -0,0 +1,3 @@
+module github.com/rjwvandenberg/simpleservers/github-webhook-server
+
+go 1.22.1
---
+++ github-webhook-server/server.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+)
+
+// Github Webhook docs:
+// https://docs.github.com/en/webhooks
+// https://docs.github.com/en/webhooks/using-webhooks/delivering-webhooks-to-private-systems
+
+func main() {
+	log.Println("Yap")
+}
